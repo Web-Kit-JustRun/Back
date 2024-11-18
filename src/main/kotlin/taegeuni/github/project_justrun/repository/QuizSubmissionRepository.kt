e2f07261b543fae3ebde728389d70f6ba4ed1bb7
TaeGueni <qoxorms1504@gmail.com>
2024-11-18 13:55:41 +0900

    🔥필요없는 코드 삭제

--- src/main/kotlin/taegeuni/github/project_justrun/repository/QuizSubmissionRepository.kt
+++ src/main/kotlin/taegeuni/github/project_justrun/repository/QuizSubmissionRepository.kt
@@ -2,22 +2,15 @@ package taegeuni.github.project_justrun.repository
 
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
-import org.springframework.data.repository.query.Param
 import taegeuni.github.project_justrun.entity.QuizSubmission
 import taegeuni.github.project_justrun.entity.User
 import taegeuni.github.project_justrun.entity.Quiz
 
 interface QuizSubmissionRepository : JpaRepository<QuizSubmission, Int> {
-    fun existsByQuizAndStudentAndIsCorrect(quiz: Quiz, student: User, isCorrect: Boolean): Boolean
-
     @Query("""
         SELECT qs FROM QuizSubmission qs
         WHERE qs.quiz = :quiz AND qs.student = :student AND qs.isCorrect = true
     """)
-    fun findFirstCorrectSubmission(
-        @Param("quiz") quiz: Quiz,
-        @Param("student") student: User
-    ): QuizSubmission?
 
     fun findByQuizAndStudent(quiz: Quiz, student: User): QuizSubmission?
 }
