fb0a1dab06c55304b346aa445db739c6f484878a
MaiT <maitmus@gmail.com>
2024-11-21 01:24:09 +0900

    feat: 회원가입 구현 완료

--- src/main/kotlin/taegeuni/github/project_justrun/controller/AuthController.kt
+++ src/main/kotlin/taegeuni/github/project_justrun/controller/AuthController.kt
@@ -4,8 +4,13 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import taegeuni.github.project_justrun.dto.CommonResponse
+import taegeuni.github.project_justrun.dto.ErrorResponse
+import taegeuni.github.project_justrun.entity.User
+import taegeuni.github.project_justrun.entity.UserType
 import taegeuni.github.project_justrun.repository.UserRepository
 import taegeuni.github.project_justrun.util.JwtUtil
+import java.time.LocalDate
 
 @RestController
 @RequestMapping("/api/auth")
@@ -14,11 +19,33 @@ class AuthController(
     private val jwtUtil: JwtUtil,
     private val passwordEncoder: BCryptPasswordEncoder
 ) {
+    @PostMapping("/register")
+    fun register(@RequestBody body: RegisterRequestDto): ResponseEntity<*> {
+        val user = userRepository.findByUsernameOrEmail(body.username, body.email)
+        if (user != null) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).body(ErrorResponse("User already exists"))
+        }
+
+        val latestStudent = userRepository.findTopByStudentNumberIsNotNullAndUserTypeOrderByUserIdDesc()
+        val studentNumber = LocalDate.now().year.toString() + String.format("%04d", latestStudent?.studentNumber?.drop(4)?.toInt()?.plus(1) ?: 1)
+
+        val newUser = User(
+            username = body.username,
+            email = body.email,
+            password = passwordEncoder.encode(body.password),
+            name = body.name,
+            userType = UserType.student,
+            studentNumber = studentNumber
+        )
+        userRepository.save(newUser)
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(CommonResponse("User registered successfully"))
+    }
 
     @PostMapping("/login")
     fun login(@RequestBody request: LoginRequest): ResponseEntity<Any> {
         val user = userRepository.findByUsername(request.username)
-            ?: return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password")
+            ?: return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ErrorResponse("Invalid username or password"))
 
         return if (passwordEncoder.matches(request.password, user.password)) {
             val token = jwtUtil.generateToken(user.userId) // 수정된 부분
@@ -36,12 +63,19 @@ class AuthController(
             )
             ResponseEntity.ok(response)
         } else {
-            ResponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid username or password")
+            ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(ErrorResponse("Invalid username or password"))
         }
     }
 }
 
 data class LoginRequest(val username: String, val password: String)
+
+data class RegisterRequestDto(
+    val username: String,
+    val password: String,
+    val email: String,
+    val name: String,
+)
 
 data class LoginResponse(
     val message: String,
