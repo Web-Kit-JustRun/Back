4dce1b32a7ca4e774b6f6baed2d330b00f55b416
MaiT <maitmus@gmail.com>
2024-11-22 10:09:20 +0900

    fix: Unused Import

--- src/main/kotlin/taegeuni/github/project_justrun/dto/QuizAttemptRequest.kt
+++ src/main/kotlin/taegeuni/github/project_justrun/dto/QuizAttemptRequest.kt
@@ -1,8 +1,5 @@
 package taegeuni.github.project_justrun.dto
 
-import com.fasterxml.jackson.annotation.JsonProperty
-
 data class QuizAttemptRequest(
-//    @JsonProperty("selected_choice")
     val selectedChoice: Int
 )
